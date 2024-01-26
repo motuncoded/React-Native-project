d868cfb5609db26d862f03eab516ced210ed01b3
motuncoded <motunrayoadeneye@example.com>
2024-01-26 20:46:53 +0100

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to React Native</Text>
+      <Text>My name is Motunrayo Adeneye.</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +15,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
