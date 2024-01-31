0ebf43caa0e0701ac7f0d68ef4fa771eb983db0a
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2024-01-31 07:13:11 +0100

    Update TextInputA.js

--- components/TextInputA.js
+++ components/TextInputA.js
@@ -2,10 +2,20 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const TextInputA = () => {
+  const [firstName onchangeFirstName] = useState("")
   return (
     <ScrollView KeyboardDismissMode="on-drag"  style={styles.container}>
       <Text style={styles.header}>TextInput with KeyboardDismissMode</Text>
       <Text>The keyboardDismissMode is set in the ScrollView, its prop by default has the value none, which means the drags do not dismiss the keyboard and can be change to on-drag prop to ensure the keyboard dismisses when scrolling or draging</Text>
+  <TextInput value={firstName}
+    placeholder="firstName"
+      onchangeText={onchangeFirstName}/>
+        <TextInput value={firstName}
+    placeholder="firstName"
+      onchangeText={onchangeFirstName}/>
+    <TextInput value={firstName}
+    placeholder="firstName"
+      onchangeText={onchangeFirstName}/>
     </Scroll View>
   )
 }
