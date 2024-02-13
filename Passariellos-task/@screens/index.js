2e07b811d7c4b24a2141c36dba2f8c37a0b688bd
Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>
2024-02-13 05:54:24 +0100

    updated

---
+++ Passariellos-task/@screens/index.js
@@ -0,0 +1,14 @@
+
+import Home from './Home';
+import Menu from './Menu';
+import Location from './Location';
+import More from './More';
+
+
+
+export {
+    Home,
+    Location,
+    Menu,
+    More
+}
---
+++ Passariellos-task/App.js
@@ -0,0 +1,85 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Home, More, Menu, Location } from "./@screens";
+import { AntDesign } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+
+
+export default function App() {
+
+  const screenOptions = {
+    tabBarShowLabel: true,
+    headerShown: false,
+    tabBarStyle: {
+      position: "absolute",
+      bottom:0,
+      right:0,
+      left: 0,
+      height: 80,
+      background:"#fafafa"
+    }
+  }
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return (
+                <View style={{ alignSelf: "center" }}>
+              <AntDesign name="home" size={24} color={focused ? "#ff6700" :"#0a0a0a"} />
+                  <Text style={{ fontSize: 12 }} color={focused ? "#ff6700" :"#0a0a0a"}>Home</Text>
+          </View> 
+              )
+           
+            }}}/>
+        <Tab.Screen name="Menu & Order" component={Menu}
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return (
+                <View>
+                      <MaterialIcons name="restaurant-menu" size={24} color={focused ? "#ff6700" :"#0a0a0a"} />
+                  <Text style={{ fontSize: 12 }} color={focused ? "#ff6700" :"#0a0a0a"}>Menu & Order</Text>
+
+                </View>
+              )
+            }
+          }} />
+        <Tab.Screen name="Location" component={Location } 
+        options={{
+            tabBarIcon: ({ focused }) => {
+              return (
+                <View>
+                              <Ionicons name="location" size={24} color={focused ? "#ff6700" :"#0a0a0a"} />
+                  <Text style={{ fontSize: 12 }} color={focused ? "#ff6700" :"#0a0a0a"}>Location</Text>
+
+                </View>
+              )
+            }
+          }} 
+/>
+        <Tab.Screen name="More" component={More } options={{
+            tabBarIcon: ({ focused }) => {
+              return (
+                <View>
+                              <Ionicons name="location" size={24} color={focused ? "#ff6700" :"#0a0a0a"} />
+                  <Text style={{ fontSize: 12 }} color={focused ? "#ff6700" :"#0a0a0a"}>Morw</Text>
+
+                </View>
+              )
+            }
+          }} />
+
+
+
+
+      </Tab.Navigator>
+      </NavigationContainer>
+  );
+}
+
+
