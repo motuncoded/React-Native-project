2371c9a2fba17af33c2bd338ac351eb5b4f432a1
Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>
2024-03-16 09:48:39 +0100

    feat: create dependency for the project

---
+++ 100-screens/@screens/Screen3.js
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Screen3 = () => {
+  return (
+      <View style={{flex:1}}>
+          <View style={{backgroundColor:"blue"}}>
+             <Text>sdfgh</Text> 
+          </View>
+          <View>
+              
+          </View>
+    </View>
+  )
+}
+
+export default Screen3
